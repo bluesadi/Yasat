74703ddf1a9aa17e7e4049aa9fbd70482056aa30
bluesadi <yiboliume@gmail.com>
2023-06-04 16:26:38 +0800

    Update README.md

--- tests/common.py
+++ tests/common.py
@@ -7,7 +7,7 @@ from Yasat.analyses.backward_slicing.criteria_selector import ArgumentSelector
 
 def run_backward_slicing_on_binary(path, sink_func="sink", arg_idx=0, cast_to=int):
     assert os.path.isfile(path)
-    assert cast_to in [int, str]
+    assert cast_to in [int, str, bool]
     subject: Subject = angr.Project(path, load_options={"auto_load_libs": False}).kb.subject
     subject.cfg
     main_addr = subject.resolve_local_function("main")
